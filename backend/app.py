4036ce1a46ff2154dd797252aafce2a57bff2965
王炜 <434533508@qq.com>
2022-09-27 23:39:23 +0800

    add router

--- backend/app.py
+++ backend/app.py
@@ -25,6 +25,13 @@ class Text(db.Model):
     def __repr__(self):
         return f"<Text {self.text}>"
 
+@app.route('/healthy')
+def healthy():
+    return {"healthy": True}, 200
+
+@app.route('/host_name')
+def host_name():
+    return {"host_name": os.environ.get('HOSTNAME')}, 200
 
 @app.route('/fetch')
 def fetch():
