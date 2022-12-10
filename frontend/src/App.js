241481c6c69febd438ad9c6aac5397cccfa192ed
王炜 <434533508@qq.com>
2022-12-10 16:16:26 +0800

    add helm env

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -46,7 +46,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hi! I'm from {process.env.NAMESPACE} namespace!
+        Hi! I'm from {process.env.NAMESPACES} namespace!
       </header>
       <div className="Grid">
         <div className="Grid-Item">
